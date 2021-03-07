d6fe3759e9768bdcf39265fd9fc942f93193238d
Max <maxim.macari.dam@gmail.com>
2021-03-07 19:04:44 +0100

    Initial Commit

---
+++ VerticalPageTab/VerticalPageTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalPageTabApp.swift
+//  VerticalPageTab
+//
+//  Created by Maxim Macari on 7/3/21.
+//
+
+import SwiftUI
+
+@main
+struct VerticalPageTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
